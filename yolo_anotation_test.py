a69566b4141cde257ce12b7ccc8a2fc500a88385
ReiKane <monstbrassband@gmail.com>
2025-07-22 00:07:27 +0900

    tested another model

--- yolo_anotation_test.py
+++ yolo_anotation_test.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-#model = YOLO('yolo11n.pt')
-model = YOLO('yolo11l.pt')
+model = YOLO('yolo11n.pt')
+#model = YOLO('yolo11l.pt')
 
-results = model.train(data='datasets/data.yaml', epochs = 10, imgsz = 640)
+results = model.train(data='datasets/data.yaml', epochs = 100, imgsz = 640)
