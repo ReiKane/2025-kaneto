c61ba808d92e774c47acaa5d36024b2776847abe
ReiKane <monstbrassband@gmail.com>
2025-07-22 01:40:14 +0900

    whole detect

--- yolo_anotation_test.py
+++ yolo_anotation_test.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO('yolo11n.pt')
 #model = YOLO('yolo11l.pt')
 
-results = model.train(data='datasets/data.yaml', epochs = 100, imgsz = 640)
+results = model.train(data='datasets_2/data.yaml', epochs = 100, imgsz = 640)
--- yolo_test_2.py
+++ yolo_test_2.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("runs/detect/train2/weights/best.pt")
+model = YOLO("runs/detect/train3/weights/best.pt")
 cap = cv2.VideoCapture("../piano_test.mp4")
 
 while True:
@@ -11,7 +11,7 @@ while True:
         break
     
     # 物体検出（動画ではtrackメソッドを利用する）
-    results = model.track(frame, iou=0.1, classes=[1], max_det=7, persist=True)
+    results = model.track(frame, persist=True)
     # 物体検出（静止画ではdetectメソッドを利用する）
     # results = model.detect("path_to_image.jpg")
     
